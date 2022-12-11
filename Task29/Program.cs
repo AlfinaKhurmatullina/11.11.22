092d1f107c6d882052c5b8705a547136027bb5d5
Alfina <afina1694@gmail.com>
2022-12-11 23:20:25 +0300

    Добавлены папки Task27,29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая 
+// 1. задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите массив чисел: ");
+int[] array = new int[8];
+int len = array.Length;
+int index = 0;
+while (index < len)
+{
+    array[index] = int.Parse(Console.ReadLine());
+    index++;
+}
+for (int i = 0; i < len; i++)
+{
+    Console.Write(array[i]);
+}
+Console.Write(Convert.ToInt32('1'));
+
