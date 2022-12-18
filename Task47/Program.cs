c16a8605de03e1bbe7e651286b7fd11f8d615ce4
Alfina <afina1694@gmail.com>
2022-12-18 18:48:39 +0300

    Добавлен папки Task47,50

---
+++ Task47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = 5;
+int n = 5;
+double[,] newArray = new double[m, n];
+
+double[,] FillArray(double[,] array01)
+{
+    for (int i = 0; i < array01.GetLength(0); i++)
+    {
+        for (int j = 0; j < array01.GetLength(1); j++)
+        {
+            array01[i, j] = Math.Round((new Random().NextDouble() * 100), 2);
+        }
+    }
+    return array01;
+}
+
+void PrintArray(double[,] array02)
+{
+    for (int i = 0; i < array02.GetLength(0); i++)
+    {
+        for (int j = 0; j < array02.GetLength(1); j++)
+        {
+            Console.Write($"{array02[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(FillArray(newArray));
---
+++ Task50/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого элемента в массиве нет
+
+Console.WriteLine("Введите индекс строки: ");
+int inputM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца: ");
+int inputN = Convert.ToInt32(Console.ReadLine());
+
+int m = 5;
+int n = 5;
+int[,] newArray = new int[m, n];
+
+int[,] FillArray(int[,] array01)
+{
+    for (int i = 0; i < array01.GetLength(0); i++)
+    {
+        for (int j = 0; j < array01.GetLength(1); j++)
+        {
+            array01[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array01;
+}
+
+void PrintArray(int[,] array02)
+{
+    for (int i = 0; i < array02.GetLength(0); i++)
+    {
+        for (int j = 0; j < array02.GetLength(1); j++)
+        {
+            Console.Write($"{array02[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(FillArray(newArray));
+
+void FindElement(int[,] array03, int posM, int posN)
+{
+    if (posM < array03.GetLength(0) && posM >= 0 && posN < array03.GetLength(1) && posN >= 0)
+    {
+        Console.WriteLine($"Элемент с позицией: строка {posM}, столбец {posN} равен {array03[posM, posN]}");
+    }
+    else
+    {
+        Console.WriteLine($"Элемента с позицией: строка {posM}, столбец {posN} в массиве нет");
+    }
+}
+FindElement(newArray, inputM, inputN);
